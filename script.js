3f2d14073a177fde52ed92aff59a5efefa6b44ea
Willem Houm <willem.houm@gmail.com>
2018-06-25 11:45:57 +0200

    html finished, js init

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 "use strict"
+$(function(){
+    var elem_list = $('.input_val');
+    console.log(elem_list);
+    $('.input_val').change(function(evt){
+        console.log(evt.target.id, evt.target.value);
+    });
+
+});
